17355dff1c6c1511751fe8c42450a3d12c50504b
Vladusick <samuelfs@mail.ru>
2022-04-26 20:02:20 +0400

    Test Task NLMK

---
+++ src/TestTask/Metal.java
@@ -0,0 +1,9 @@
+package TestTask;
+
+public class Metal {
+
+    private String name;
+    public Metal(String name) {
+        this.name = name;
+    }
+}
---
+++ src/TestTask/Test.java
@@ -0,0 +1,11 @@
+package TestTask;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        Metal metal = new Metal("Сталь");
+        MultiLabCelox multiLabCelox = new MultiLabCelox();
+        multiLabCelox.obtainingOxidation(metal);
+    }
+}
